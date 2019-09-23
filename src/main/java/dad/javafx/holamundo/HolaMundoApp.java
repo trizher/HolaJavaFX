2d3f2e6de3a1ad6582a66d37b60cc2083a932b10
Beatriz Garcia <beagh92@gmail.com>
2019-09-23 10:07:22 +0100

    aplicación terminada

--- src/main/java/dad/javafx/holamundo/HolaMundoApp.java
+++ src/main/java/dad/javafx/holamundo/HolaMundoApp.java
@@ -1,7 +1,10 @@
 package dad.javafx.holamundo;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -15,9 +18,27 @@ public class HolaMundoApp extends Application {
 		saludoLabel.setText("Aquí saldrá el saludo");
 		saludoLabel.setLayoutX(20);
 		saludoLabel.setLayoutY(20);
-
+		
+		Button saludarButton = new Button();
+		saludarButton.setText("Saludar");
+		saludarButton.setLayoutX(20);
+		saludarButton.setLayoutY(80);
+//		saludarButton.setOnAction(new EventHandler<ActionEvent>() {
+//			
+//			@Override
+//			public void handle(ActionEvent event) {
+//				saludoLabel.setText("Hola JavaFX!");
+//				saludarButton.setVisible(false);
+//			}
+//		});
+		
+		saludarButton.setOnAction(event -> {
+			saludoLabel.setText("Hola JavaFX");
+			saludarButton.setVisible(false);
+		});
+		
 		Pane root = new Pane();
-		root.getChildren().add(saludoLabel);
+		root.getChildren().addAll(saludoLabel, saludarButton);
 		
 		Scene scene = new Scene(root, 320, 200);
 		
